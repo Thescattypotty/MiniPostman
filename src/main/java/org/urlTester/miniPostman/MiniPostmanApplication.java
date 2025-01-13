a38347c81ddb6d7ff47dafd87dd3070ea61c7fb3
Thescattypotty <bnsyahya123@gmail.com>
2025-01-13 17:35:22 +0100

    Init Project

---
+++ src/main/java/org/urlTester/miniPostman/MiniPostmanApplication.java
@@ -0,0 +1,13 @@
+package org.urlTester.miniPostman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPostmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/urlTester/miniPostman/MiniPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.urlTester.miniPostman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPostmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
